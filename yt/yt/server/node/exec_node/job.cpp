2b1df1ec59b714d544b93263950502bedb4152d6
Konstantin Khlebnikov <koct9i@gmail.com>
2024-05-22 11:56:33 +0200

    Map JobProxyFailed/CannotStartProcess to job abort reason "Other"
    If we haven't found any more specific errors it's save to assume
    that was a fluke and job should retry on this or other node.

--- yt/yt/server/node/exec_node/job.cpp
+++ yt/yt/server/node/exec_node/job.cpp
@@ -3099,6 +3099,9 @@ std::optional<EAbortReason> TJob::DeduceAbortReason()
     }
 
     if (auto jobProxyFailedError = resultError.FindMatching(NExecNode::EErrorCode::JobProxyFailed)) {
+        if (resultError.FindMatching(EProcessErrorCode::CannotStartProcess)) {
+            return EAbortReason::Other;
+        }
         if (auto processError = resultError.FindMatching(EProcessErrorCode::NonZeroExitCode)) {
             auto exitCode = NExecNode::EJobProxyExitCode(processError->Attributes().Get<int>("exit_code"));
             switch (exitCode) {
