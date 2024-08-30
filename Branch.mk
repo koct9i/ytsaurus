56493f2eb466bc8b08b5b2d83b50877c57006138
Andrey Chulkov <achulkov2@nebius.com>
2024-08-30 16:44:52 +0000

    Refactor branch structure
    (cherry picked from commit 89d57c33c84628f2fc6343199eb46f53b7318940)

--- Branch.mk
+++ Branch.mk
@@ -5,52 +5,42 @@ DOCKER_OVERRIDE_BASE_IMAGE ?= stable-23.2.1-relwithdebinfo
 
 YT_VERSION ?= 24.1
 
-# map current branch upstream "@{u}" to various branches
-BRANCH_STABLE_BASE = $(shell git rev-parse --symbolic-full-name @{u} | sed -E 's#(refs/remotes/[^/]+)/([^/]+)/(.*)/stable/([^/]+)(.*)#\1/stable/\4#')
-REMOTE_BOOTSTRAP_BRANCH = $(shell git rev-parse --symbolic-full-name @{u} | sed -E 's#(refs/remotes/[^/]+)/([^/]+)/(.*)/stable/([^/]+)(.*)#\1/\2/patches/bootstrap/stable/\4#')
-LOCAL_BOOTSTRAP_BRANCH = $(shell git rev-parse --symbolic-full-name @{u} | sed -E 's#(refs/remotes/[^/]+)/([^/]+)/(.*)/stable/([^/]+)(.*)#\2/patches/bootstrap/stable/\4#')
-PUBLIC_RELEASE_BRANCH = $(shell git rev-parse --symbolic-full-name @{u} | sed -E 's#(refs/remotes/[^/]+)/([^/]+)/(.*)/stable/([^/]+)(.*)#\2/releases/public/stable/\4#')
-PRIVATE_RELEASE_BRANCH = $(shell git rev-parse --symbolic-full-name @{u} | sed -E 's#(refs/remotes/[^/]+)/([^/]+)/(.*)/stable/([^/]+)(.*)#\2/releases/private/stable/\4#')
-BRANCH_FRAGMENT_PUBLIC_PREFIXES = $(shell git rev-parse --symbolic-full-name @{u} | sed -E 's#(refs/remotes/[^/]+)/([^/]+)/(.*)/stable/([^/]+)(.*)#\1/\2/patches/public/stable/\4#')
-BRANCH_FRAGMENT_PRIVATE_PREFIXES = $(shell git rev-parse --symbolic-full-name @{u} | sed -E 's#(refs/remotes/[^/]+)/([^/]+)/(.*)/stable/([^/]+)(.*)#\1/\2/patches/private/stable/\4#')
-BRANCH_PUBLIC_FRAGMENTS=$(shell git for-each-ref --sort="-authordate" "--format=%(refname)" ${BRANCH_FRAGMENT_PUBLIC_PREFIXES})
-BRANCH_PRIVATE_FRAGMENTS=$(shell git for-each-ref --sort="-authordate" "--format=%(refname)" ${REMOTE_BOOTSTRAP_BRANCH}  ${BRANCH_FRAGMENT_PRIVATE_PREFIXES})
+BRANCH_PREFIX = mirror-ytsaurus
+# (refs/remotes/<remote=origin>)/mirror-ytsaurus/(bootstrap|patches|releases)/stable/(<version>)(EOS or /<patch>)
+BRANCH_NAME_PATTERN = (refs/remotes/[^/]+)/${BRANCH_PREFIX}/([^/]+)/stable/([^/]+)($$|/.*)
+UPSTREAM = $(shell git rev-parse --symbolic-full-name @{u})
+
+BRANCH_STABLE_BASE = $(shell echo ${UPSTREAM} | sed -E 's#${BRANCH_NAME_PATTERN}#\1/stable/\3#')
+REMOTE_BOOTSTRAP_BRANCH = $(shell echo ${UPSTREAM} | sed -E 's#${BRANCH_NAME_PATTERN}#\1/${BRANCH_PREFIX}/bootstrap/stable/\3#')
+LOCAL_BOOTSTRAP_BRANCH = $(shell echo ${UPSTREAM} | sed -E 's#${BRANCH_NAME_PATTERN}#${BRANCH_PREFIX}/bootstrap/stable/\3#')
+RELEASE_BRANCH = $(shell echo ${UPSTREAM} | sed -E 's#${BRANCH_NAME_PATTERN}#${BRANCH_PREFIX}/releases/stable/\3#')
+BRANCH_FRAGMENT_PREFIXES = $(shell echo ${UPSTREAM} | sed -E 's#${BRANCH_NAME_PATTERN}#\1/${BRANCH_PREFIX}/patches/stable/\3#')
+BRANCH_FRAGMENTS = $(shell git for-each-ref --sort="-authordate" "--format=%(refname)" ${REMOTE_BOOTSTRAP_BRANCH}  ${BRANCH_FRAGMENT_PREFIXES})
 
 checkout-bootstrap-branch: ## Checkout bootstrap branch for the current release.
 	git diff --quiet
 	git diff --quiet --cached
 	git checkout ${LOCAL_BOOTSTRAP_BRANCH}
 
-checkout-public-release-branch: ## Checkout public release branch for the current release.
+checkout-release-branch: ## Checkout release branch for the current release.
 	git diff --quiet
 	git diff --quiet --cached
-	git checkout ${PUBLIC_RELEASE_BRANCH}
+	git checkout ${RELEASE_BRANCH}
 
-checkout-release-branch: ## Checkout (private) release branch for the current release.
-	git diff --quiet
-	git diff --quiet --cached
-	git checkout ${PRIVATE_RELEASE_BRANCH}
+define newline
 
-rebuild-public-release-branch: checkout-public-release-branch ## Rebuild public release branch by resetting it to the corresponding stable branch and cherry-picking fragments from mirror-ytsaurus/patches/public/stable/version/*.
+
+endef
+
+rebuild-release-branch: checkout-release-branch ## Rebuild release branch by resetting it to the corresponding stable branch and cherry-picking fragments from mirror-ytsaurus/patches/stable/version/*.
 	git reset --hard ${BRANCH_STABLE_BASE}
-	if [ ! -z "${BRANCH_PUBLIC_FRAGMENTS}" ]; then git cherry-pick -x --keep-redundant-commits ${BRANCH_PUBLIC_FRAGMENTS} --not HEAD; fi
+	$(foreach fragment,${BRANCH_FRAGMENTS},git cherry-pick -x --keep-redundant-commits ${fragment} --not HEAD $(newline))
 
-rebuild-private-release-branch: checkout-release-branch ## Rebuild private release branch by resetting it to the current public release branch and cherry-picking fragments from mirror-ytsaurus/patches/private/stable/version/*.
-	git reset --hard ${PUBLIC_RELEASE_BRANCH}
-	if [ ! -z "${BRANCH_PRIVATE_FRAGMENTS}" ]; then git cherry-pick -x --keep-redundant-commits ${BRANCH_PRIVATE_FRAGMENTS} --not HEAD; fi
-
-rebuild-release-branch: rebuild-public-release-branch rebuild-private-release-branch ## Rebuild release branch by first rebuilding the public release branch on top of the corresponding stable branch and then rebuilding the private branch on top of the resulting public release branch.
-
-push-public-release-branch: ## Push public release branch.
-	git push -f origin ${PUBLIC_RELEASE_BRANCH}
-
-push-private-release-branch: TAG_NAME=tracto-${YT_VERSION}-$(shell git show -s --pretty=%cs-%H ${PRIVATE_RELEASE_BRANCH})
-push-private-release-branch: ## Push private release branch with corresponding tag.
-	git tag -a ${TAG_NAME} -m "YTsaurus tracto ${YT_VERSION} release" ${PRIVATE_RELEASE_BRANCH}
-	git push -f origin ${PRIVATE_RELEASE_BRANCH}
+push-release-branch: TAG_NAME=tracto-${YT_VERSION}-$(shell git show -s --pretty=%cs-%H ${RELEASE_BRANCH})
+push-release-branch: ## Push release branch with corresponding tag. Should start CI.
+	git tag -a ${TAG_NAME} -m "YTsaurus tracto ${YT_VERSION} release" ${RELEASE_BRANCH}
+	git push -f origin ${RELEASE_BRANCH}
 	git push origin ${TAG_NAME}
-
-push-release-branches: push-public-release-branch push-private-release-branch ## Push public and private release branches with corresponding tags. Should start CI.
 
 release-image: DOCKER_IMAGE_TAG=tracto-${YT_VERSION}-$(shell git show -s --pretty=%cs-%H)
 release-image: hack-local-python docker-ytsaurus ## Build release docker image and push to nemax registry.
